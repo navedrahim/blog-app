5676b9d7b046f5beb5af0edf57332a1bedc23cad
Gino2B <gbaybay30@gmail.com>
2021-07-15 16:50:09 -0400

    added home screen, removed app css and added more routes in app.js

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="home-screen">
+      <h1>Poke-Bloggin</h1>
+      <h3>Latest: </h3>
+      <h4>App by: Gene, Naved, Nick</h4>
+    </div>
+  );
+}
+
+export default Home;
