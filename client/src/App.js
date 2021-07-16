589897c9d4b84eae74adb4530b65485d7f49f584
navedrahim <nrah1988@gmail.com>
2021-07-16 10:52:33 -0400

    added layout to PostCreate and un-commented routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "./screens/Home/Home.jsx";
-// import PostCreate from "./screens/PostCreate/PostCreate.jsx"
+import PostCreate from "./screens/PostCreate/PostCreate.jsx"
 import PostDetail from "./screens/PostDetail/PostDetail.jsx";
-// import PostEdit from "./screens/PostEdit/PostEdit.jsx"
+import PostEdit from "./screens/PostEdit/PostEdit.jsx"
 import Posts from "./screens/Posts/Posts.jsx";
 import "./App.css";
 
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route path="/create" component={PostCreate} /> */}
+        <Route path="/create" component={PostCreate} />
         <Route exact path="/posts/:id" component={PostDetail} />
-        {/* <Route exact path="/posts/:id/edit" component={PostEdit} /> */}
+        <Route exact path="/posts/:id/edit" component={PostEdit} />
         <Route exact path="/posts" component={Posts} />
       </Switch>
     </div>
